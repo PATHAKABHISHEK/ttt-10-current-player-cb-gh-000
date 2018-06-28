5c3ecc790c1083b67f1cca1090e1578d16f64296
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-06-28 13:59:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 def turn_count(board)
+  counter = 0
   board.each do |position|
-
+      if (position == "X" || position =="O" )
+        counter +=1
+      else 
+        counter += 0
   end
+  return counter
 end
